36a844e9c13cc4207e0187cb5375ded117969e87
Exant64 <warpcauldronproject@gmail.com>
2021-09-17 19:22:21 +0300

    njCnkCompile done and i hope i never have to look at it again, also njCreateLight

--- src/Chunk/njCnkCompile.c
+++ src/Chunk/njCnkCompile.c
@@ -146,12 +146,12 @@ void njCnkDirectVlist(Uint16* vlist, NJS_DIRECT_BUF* buf)
 void njCnkSetMaterial( Uint32 data, Uint32 *plist, Float *diff, Float *ambi, Float *spec );
 Sint32 njSetkmCnkTextureNum(Uint32 a, Uint32 b);
 void njSetkmCnkBlendMode(Uint32 p);
-Uint32	njCnkDirectPlist( Uint16* vl, NJS_DIRECT_BUF* buf, NJS_DIRECT_GLOBAL* head )
+void njSetkmColorMode(Uint32 p);
+NJS_DIRECT_HEAD*	njCnkDirectPlist( Uint16* vl, NJS_DIRECT_BUF* buf, NJS_DIRECT_GLOBAL* head, int* n, int transparent /*stack*/ )
 {
     Sint16 type;
     Uint16 size;
     Uint16 val;
-    int i = 0;
 
     while((type = *vl++) != 0x00FF)
     {
@@ -186,17 +186,270 @@ Uint32	njCnkDirectPlist( Uint16* vl, NJS_DIRECT_BUF* buf, NJS_DIRECT_GLOBAL* hea
                         type &= _nj_constant_attr_and_;
                         type |= _nj_constant_attr_or_;
                     }
-
+                    type <<= 8;
                     if(!unk_A90)
                     {
+                        NJS_DIRECT_HEAD* nextHead;
+                        NJS_DIRECT_GLOBAL* pHead = (NJS_DIRECT_GLOBAL*)head;
 
+                        Uint32 vtx = 0;
+                        Uint32 opaqueNormal, opaqueFlat, transNormal, transFlat;
+                        Uint32 flag = type;
+                        Uint32 v1 = NJD_ARC1_HIGHLIGHT;
+                        if(type & 2) //ignore specular?
+                            v1 = 0;
+                        transNormal = v1 | NJD_ARC1_TRANS | NJD_ARC1_INTENSITY | NJD_ARC1_TEXTURE;
+                        transFlat = v1 | NJD_ARC1_TRANS | NJD_ARC1_INTENSITY | NJD_ARC1_TEXTURE_FLAT;
+                        opaqueNormal = v1 | NJD_ARC1_OPAQUE | NJD_ARC1_INTENSITY | NJD_ARC1_TEXTURE;
+                        opaqueFlat = v1 | NJD_ARC1_OPAQUE | NJD_ARC1_INTENSITY | NJD_ARC1_TEXTURE_FLAT;
+                        switch(data)
+                        {
+                            case NJD_CS_UVN:
+                            case NJD_CS_UVH:
+                                if(type & 8) //usealpha
+                                {
+                                    if(transparent == 1)
+                                    {
+                                        
+                                        *n += 1;
+                                        if(type & 0x40) //env
+                                        {
+                                            if(type & 0x20) //flat
+                                                njSetkmColorMode(transFlat);
+                                            else
+                                                njSetkmColorMode(transNormal);
+                                            pHead->mode = 1;
+                                        }
+                                        else
+                                        {
+                                            if(type & 0x20) //flat
+                                                njSetkmColorMode(v1 | NJD_ARC1_TRANS | NJD_ARC1_TEXTURE_FLAT | NJD_ARC1_UV16_FLOAT);
+                                            else
+                                                njSetkmColorMode(v1 | NJD_ARC1_TRANS | NJD_ARC1_TEXTURE | NJD_ARC1_UV16_FLOAT);
+                                            pHead->mode = 0;
+                                        }
+                                        nextHead = njCnkDirectGlobalTrans(head);
+                                    }
+                                }
+                                else
+                                {
+                                    if(!transparent)
+                                    {
+                                        *n += 1;
+                                        if(type & 0x40) //env
+                                        {
+                                            if(type & 0x20) //flat
+                                                njSetkmColorMode(opaqueFlat);
+                                            else
+                                                njSetkmColorMode(opaqueNormal);
+                                            pHead->mode = 1;
+                                        }
+                                        else
+                                        {
+                                            if(type & 0x20) //flat
+                                                njSetkmColorMode(v1 | NJD_ARC1_OPAQUE | NJD_ARC1_INTENSITY | NJD_ARC1_TEXTURE_FLAT | NJD_ARC1_UV16_FLOAT);
+                                            else
+                                                njSetkmColorMode(v1 | NJD_ARC1_OPAQUE | NJD_ARC1_INTENSITY | NJD_ARC1_TEXTURE | NJD_ARC1_UV16_FLOAT);
+                                            pHead->mode = 0;
+                                        }
+                                        nextHead = njCnkDirectGlobalOpaque(head);
+                                    }
+                                }
+                                if(data == 65)
+                                    head = njCnkDirectTexture(vl,buf,val,nextHead,0.00390625, &vtx);
+                                else
+                                    head = njCnkDirectTexture(vl,buf,val,nextHead,0.0009765625, &vtx);
+                                pHead->num = vtx;
+                                break;
+                            case NJD_CS:
+                                if(type & 8) //usealpha
+                                {
+                                    if(transparent == 1)
+                                    {
+                                        *n += 1;
+                                        if(type & 0x40) //env
+                                        {
+                                            if(type & 0x20) //flat
+                                                njSetkmColorMode(transFlat);
+                                            else
+                                                njSetkmColorMode(transNormal);
+                                            pHead->mode = 1;
+                                        }
+                                        else
+                                        {
+                                            if(type & 0x20) //flat
+                                                njSetkmColorMode(v1 | NJD_ARC1_TRANS | NJD_ARC1_INTENSITY);
+                                            else
+                                                njSetkmColorMode(v1 | NJD_ARC1_TRANS | NJD_ARC1_INTENSITY | NJD_ARC1_NORMAL);
+                                            pHead->mode = 0;
+                                        }
+                                        nextHead = njCnkDirectGlobalTrans(head);
+                                    }
+                                }
+                                else
+                                {
+                                    if(!transparent)
+                                    {
+                                        *n += 1;
+                                        if(type & 0x40) //env
+                                        {
+                                            if(type & 0x20) //flat
+                                                njSetkmColorMode(opaqueFlat);
+                                            else
+                                                njSetkmColorMode(opaqueNormal);
+                                            pHead->mode = 1;
+                                        }
+                                        else
+                                        {
+                                            if(type & 0x20) //flat
+                                                njSetkmColorMode(v1 | NJD_ARC1_OPAQUE | NJD_ARC1_INTENSITY);
+                                            else
+                                                njSetkmColorMode(v1 | NJD_ARC1_OPAQUE | NJD_ARC1_INTENSITY | NJD_ARC1_NORMAL);
+                                            pHead->mode = 0;
+                                        }
+                                        nextHead = njCnkDirectGlobalOpaque(head);
+                                    }
+                                }
+                            
+                                head = njCnkDirectPolygon(vl,buf,val,nextHead, &vtx);
+                                pHead->num = vtx;
+                                break;
+                        }
+                        _nj_direct_vertex_cnt_ += vtx;
                     }
                     else
                     {
+                        NJS_DIRECT_HEAD* nextHead;
+                        NJS_DIRECT_GLOBAL_D8* pHead = (NJS_DIRECT_GLOBAL_D8*)head;
 
+                        Uint32 vtx = 0;
+                        Uint32 flag = type;
+                        switch(data)
+                        {
+                            case NJD_CS_UVN:
+                            case NJD_CS_UVH:
+                                if(type & 8) //usealpha
+                                {
+                                    if(transparent == 1)
+                                    {
+                                        *n += 1;
+
+                                        if(type & 0x20) //flat
+                                            njSetkmColorMode(NJD_ARC1_TRANS | NJD_ARC1_TEXTURE_FLAT | NJD_ARC1_UV16_FLOAT);
+                                        else
+                                            njSetkmColorMode(NJD_ARC1_TRANS | NJD_ARC1_TEXTURE | NJD_ARC1_UV16_FLOAT);
+                                        
+                                        nextHead = njCnkDirectGlobalTransD8(pHead);
+
+                                        if(type & 0x40)
+                                        {
+                                            head = njCnkDirectTextureEnvD8(vl,buf,val,nextHead,&vtx);                   
+                                            pHead->mode = 1;
+                                        }
+                                        else
+                                        {
+                                            if(data == 65)
+                                                head = njCnkDirectTextureD8(vl,buf,val,nextHead,0.00390625,    &vtx);
+                                            else
+                                                head = njCnkDirectTextureD8(vl,buf,val,nextHead,0.0009765625,  &vtx);
+                                            pHead->mode = 0;
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if(!transparent)
+                                    {
+                                        *n += 1;
+
+                                        if(type & 0x20) //flat
+                                            njSetkmColorMode(NJD_ARC1_OPAQUE | NJD_ARC1_TEXTURE_FLAT | NJD_ARC1_UV16_FLOAT);
+                                        else
+                                            njSetkmColorMode(NJD_ARC1_OPAQUE | NJD_ARC1_TEXTURE | NJD_ARC1_UV16_FLOAT);
+  
+                                        nextHead = njCnkDirectGlobalOpaqueD8(pHead);
+
+                                        if(type & 0x40)
+                                        {
+                                            head = njCnkDirectTextureEnvD8(vl,buf,val,nextHead,&vtx);                   
+                                            pHead->mode = 1;
+                                        }
+                                        else
+                                        {
+                                            if(data == 65)
+                                                head = njCnkDirectTextureD8(vl,buf,val,nextHead,0.00390625,    &vtx);
+                                            else
+                                                head = njCnkDirectTextureD8(vl,buf,val,nextHead,0.0009765625,  &vtx);
+                                            pHead->mode = 0;
+                                        }
+
+                                    }
+                                }
+                                
+                                pHead->num = vtx;
+                                _nj_direct_vertex_cnt_ += vtx;
+                                break;
+                            case NJD_CS:
+                                if(type & 8) //usealpha
+                                {
+                                    if(transparent == 1)
+                                    {
+                                        
+                                        *n += 1;
+
+                                        if(type & 0x20) //flat
+                                            njSetkmColorMode(NJD_ARC1_TRANS | NJD_ARC1_NORMAL_FLAT);
+                                        else
+                                            njSetkmColorMode(NJD_ARC1_TRANS | NJD_ARC1_NORMAL);
+                                        
+                                        nextHead = njCnkDirectGlobalTransD8(pHead);
+
+                                        if(type & 0x40)
+                                        {
+                                            head = njCnkDirectPolygonEnvD8(vl,buf,val,nextHead,&vtx);                   
+                                            pHead->mode = 1;
+                                        }
+                                        else
+                                        {
+                                            head = njCnkDirectPolygonD8(vl,buf,val,nextHead, &vtx);
+                                            pHead->mode = 0;
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if(!transparent)
+                                    {
+                                        *n += 1;
+
+                                        if(type & 0x20) //flat
+                                            njSetkmColorMode(NJD_ARC1_OPAQUE | NJD_ARC1_NORMAL_FLAT);
+                                        else
+                                            njSetkmColorMode(NJD_ARC1_OPAQUE | NJD_ARC1_NORMAL);
+  
+                                        nextHead = njCnkDirectGlobalOpaqueD8(pHead);
+
+                                        if(type & 0x40)
+                                        {
+                                            head = njCnkDirectPolygonEnvD8(vl,buf,val,nextHead,&vtx);                   
+                                            pHead->mode = 1;
+                                        }
+                                        else
+                                        {
+                                            head = njCnkDirectPolygonD8(vl,buf,val,nextHead, &vtx);
+                                            pHead->mode = 0;
+                                        }
+
+                                    }
+                                }
+                                
+                                pHead->num = vtx;
+                                _nj_direct_vertex_cnt_ += vtx;
+                                break;
+                        }
+                        _nj_direct_vertex_cnt_ += vtx;
                     }
 
-                    _nj_direct_vertex_cnt_++;
                     (Uint8*)vl += ((size-1) * 2);       
                 }
                 
@@ -204,30 +457,42 @@ Uint32	njCnkDirectPlist( Uint16* vl, NJS_DIRECT_BUF* buf, NJS_DIRECT_GLOBAL* hea
                 
         }
     }
-    return i;
+    return head;
 }
 
 void* _njCnkDirectModelCompile( NJS_CNK_MODEL* mdl, void* ptr)
 {
+    int nOP = 0;
+    int nTR = 0;
+    NJS_DIRECT_HEAD* nextHead;
     NJS_DIRECT_HEAD* head = (NJS_DIRECT_HEAD*)ptr;
     unk_A9C = head;
 
     njCalcPoint(0, &mdl->center, (NJS_POINT3*)&head->x);
     head->r = mdl->r;
     head++;
+    nextHead = head;
     {
         Uint16* vlist = (Uint16*)mdl->vlist;
         if(vlist)
             njCnkDirectVlist(vlist, (NJS_DIRECT_BUF*)_nj_vertex_buf_);
     }  
-    //plist calls
+    
     {
         Uint32* plist = mdl->plist;
-        njCnkDirectPlist(plist, (NJS_DIRECT_BUF*)_nj_vertex_buf_,head);
+        if(plist)
+        {
+            NJS_DIRECT_HEAD* opHead = (NJS_DIRECT_HEAD*)njCnkDirectPlist(plist, (NJS_DIRECT_BUF*)_nj_vertex_buf_,head,&nOP,0);
+            nextHead = (NJS_DIRECT_HEAD*)njCnkDirectPlist(plist, (NJS_DIRECT_BUF*)_nj_vertex_buf_,opHead,&nTR,1);
+        }
     }
 
+    head->next = nextHead;
+    head->nOP = nOP;
+    head->nTR = nTR;
     head->mode = unk_A90;
-    
+
+    return nextHead;
 }
 
 void _njCnkDirectObjectCompile( NJS_CNK_OBJECT *obj )
